af40a5bd5c980254b5b0acd36d21ff5e373bdbd0
lllssslll2022 <235221458@qq.com>
2026-07-06 11:05:44 +0800

    修改

--- Server/src/main/java/online/mushu/server/Vo/Login.java
+++ Server/src/main/java/online/mushu/server/Vo/Login.java
@@ -15,14 +15,14 @@ enum LICoda {
     FailureRequest(401);
 
 
-    private final int coda;
+    private final int code;
 
-    LICoda(int coda) {
-        this.coda = coda;
+    LICoda(int code) {
+        this.code = code;
     }
 
-    public int getCoda() {
-        return coda;
+    public int getCode() {
+        return code;
     }
 
 
@@ -35,14 +35,14 @@ public record Login(String username, int ID, int coda, String JWT) {
     }
 
     public static Login Success(String username, int ID, String JWT) {
-        return new Login(username, ID, LICoda.Success.getCoda() , JWT);
+        return new Login(username, ID, LICoda.Success.getCode() , JWT);
     }
 
     public static Login Failure() {
-        return new Login("", 0, LICoda.Failure.getCoda(), "");
+        return new Login("", 0, LICoda.Failure.getCode(), "");
     }
 
     public static Login FailureRequest(String username, int ID) {
-        return new Login(username, ID, LICoda.FailureRequest.getCoda(), null);
+        return new Login(username, ID, LICoda.FailureRequest.getCode(), null);
     }
 }
